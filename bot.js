b07ed0d5213fcb01092388275bd7f92e72907de0
sreekar9601 <sreekarsarvepalli9601@gmail.com>
2021-10-11 12:03:39 +0530

    Bot complete, returns crypto data

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const { Client } =  require('discord.js');
 const dotenv = require('dotenv');
+const axios = require('axios');
 dotenv.config();//using dotenv to load environment variables from ./.env
 const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });//create new bot instance
 bot.login(process.env.DISCORD_BOT_TOKEN);//functionality of discordjs
@@ -14,5 +15,25 @@ bot.on('message',async (message)=>{
     if(message.content.startsWith('!ping')){
         return message.reply('I am working');
     }
+    //price commands starts here
+    if(message.content.startsWith('!price')){
+        const[command, ...args] = message.content.split(' ');
+        if(args.length!==2){
+            return message.reply('Enter crypto and currency to compare with!');
+        }
+        else{
+            const[coin, vsCurrency] = args;
+            try{
+                const{data} = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${vsCurrency}`);
+                if(!data[coin][vsCurrency])throw Error();
+                return message.reply(`The current price of 1 ${coin} = ${data[coin][vsCurrency]} ${vsCurrency}`);
+
+            }
+            catch(err){
+                return message.reply('Please check input values correctly');
+            }
+        }
+    }
 });
+
 
