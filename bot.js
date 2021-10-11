73f741ea5f0dde08433af9fb9c04572aa7e75f73
sreekar9601 <sreekarsarvepalli9601@gmail.com>
2021-10-11 11:28:45 +0530

    added ping command response

--- bot.js
+++ bot.js
@@ -4,3 +4,15 @@ dotenv.config();//using dotenv to load environment variables from ./.env
 const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });//create new bot instance
 bot.login(process.env.DISCORD_BOT_TOKEN);//functionality of discordjs
 
+bot.on('ready',()=>{
+    console.log('${bot.user.username} is now online!')
+});// logs 'bot is now online' when logged in
+
+bot.on('message',async (message)=>{
+    if(message.author.bot)return;
+
+    if(message.content.startsWith('!ping')){
+        return message.reply('I am working');
+    }
+});
+
